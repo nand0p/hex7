2f886ab5bc2089e66ebdd6ffbabade674e5b583e
Fernando J Pando <fernando.pando@stelligent.com>
2016-01-28 00:03:31 -0500

    track

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
     date_default_timezone_set('America/New_York');
     error_reporting(E_ERROR | E_WARNING | E_PARSE);
-    $num = rand(1000000,99999999);
-    $id = "E" . chr(65 + mt_rand(0, 5));
-    $visnum = $id . $num;
     $bgcolor="#FF0000";
     $linkcolor="#FFFFFF";
     $linkcolor="red";
@@ -18,7 +15,6 @@
     $width = 65;
     $height = 175;
     $density = 80;
-    $whitespace = 50;
     exec("whois $ip | egrep -i 'net|name' | grep -v arin | grep -v ripe | grep -v abuse ",$who);
     echo "<html><head><title>Hex 7 Internet Solutions</title>";
     if (isset($_GET['refresh']) && ($_GET['refresh'] != "off"))
@@ -26,17 +22,13 @@
     echo "
         <link rel=icon href=\"http://www.hex7.com/favicon.ico\" >
         <link rel=\"shortcut icon\" href=\"http://www.hex7.com/favicon.ico\" >
-        <script type=\"text/javascript\">
-            var _gaq = _gaq || [];
-            _gaq.push(['_setAccount', 'UA-32710227-1']);
-            _gaq.push(['_trackPageview']);
-            (function() {
-                var ga = document.createElement('script');
-                ga.type = 'text/javascript'; ga.async = true;
-                #ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
-                #ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-            }) ();
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-32710227-1', 'auto');
+            ga('send', 'pageview');
         </script>
         <style>
         a { text-decoration: none; color: black; }
@@ -63,27 +55,17 @@
         }
         echo "</td></tr>";
     }
-    echo "</table>";
-    for ($j = 0; $j < $whitespace; $j++) { echo "&nbsp;"; }
     echo "
-        </td></tr></table>
+        </table>
         <center><br><p><br>
-            <a href=http://serverfault.com/users/190933/nandop>
-            <img src=http://serverfault.com/users/flair/190933.png?theme=hotdog
-                     width=95% alt=serverfault title=serverfault>
-            </a>
         <p><br><p>
         <table border=0 width=100%><tr>
-        <td><img src=xtree.gif width=150></td>
-        <td width=40% align=center>
-        <b><font size=5>&copy;2000-2016</font></b><br>
-        <a target=_blank href=http://hex7.com>
-        <b><font color=red size=+2>Hex 7 Internet Solutions</font></b></a></td>
-        <td><img src=heart.gif width=100></td>
-        <td width=40% align=center><b><font size=5>VIN:</font></b><br>
-        <font color=red size=+2><b>_-_&nbsp;&nbsp; $visnum &nbsp;&nbsp;_-_</b></font>
-        </td><td><img src=xtree.gif width=150></td>
-        </td></tr></table>
+          <td width=40% align=center>
+            <b><font size=5>&copy;2000-2016</font></b><br>
+            <a target=_blank href=http://hex7.com>
+            <b><font color=red size=+2>Hex 7 Internet Solutions</font></b></a>
+          </td>
+        </tr></table>
         </center>$os :: <a href=http://en.wikipedia.org/wiki/SOS>s0s</a> at hex7 d0t c0m
         </body>
         </html>
