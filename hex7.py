740a80817dc2f55f35f951fb43c6093d3208663e
Fernando J Pando <fernando.pando@stelligent.com>
2016-09-19 00:25:22 -0400

    fix favicon

--- hex7.py
+++ hex7.py
@@ -26,7 +26,7 @@ def _head():
     _textcolor = "black"
     _html.extend([ "<html><head><title>Hex 7 Internet Solutions</title>",
                    "<meta http-equiv=refresh content=20>",
-                   "<link rel=icon href=http://www.hex7.com/favicon.ico>",
+                   "<link rel=\"shortcut icon\" href=https://s3-us-west-1.amazonaws.com/hex7/favicon.ico>",
                    "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){",
                    "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),",
                    "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)",
