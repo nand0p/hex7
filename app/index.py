d3f69d9d494a09219310c001f533a5ecd4754972
nando <nando@hex7.com>
2021-08-29 15:41:44 -0400

    more x headers

--- app/index.py
+++ app/index.py
@@ -79,7 +79,7 @@ def _google():
 
 def _rezo():
     _html = []
-    _ip = _get_ip()
+    _ip = _get_ip()[0]
     _html.extend([ '<center><h1 name=ip>', _ip, '</h1><br>' ])
     if _ip != '127.0.0.1' and _ip != '172.17.0.1':
         if not IPAddress(_ip).is_private():
@@ -97,8 +97,7 @@ def _get_ip():
     if request.headers.getlist("X-Forwarded-For"):
         return request.headers.getlist("X-Forwarded-For")[0]
     else:
-        return request.remote_addr[0]
-        #return request.remote_addr
+        return request.remote_addr
 
 def _art():
     _html = []
